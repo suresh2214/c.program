56958e3311bfb5346356a1d5e48b0aff96eed847
suresh2214 <babusureshoruganti@gmail.com>
2024-10-25 12:02:45 +0530

    Create negative.c

---
+++ negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the n");
+    scanf("%d%",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    else
+    {
+        printf("negative");
+    }
+}
