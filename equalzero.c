98ff940ef1979314c3681a4927fea81ffe728996
suresh2214 <babusureshoruganti@gmail.com>
2024-10-25 12:09:40 +0530

    Create equalzero.c

---
+++ equalzero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+        }
+        else if(n=0)
+        {
+            printf("zero");
+        }
+        else
+        {
+            printf("negative");        }
+        }
+        
+    
